cf706919ddaac4f4608181cf1f9a131dc0b09634
Fatih Karatepe <theapache.58@gmail.com>
2026-03-05 00:23:45 +0300

    morgan logger date bug fixed.

--- src/config/morgan-logger.ts
+++ src/config/morgan-logger.ts
@@ -6,9 +6,8 @@ const logDirectory = path.join(process.cwd(), 'logs');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 export const accessLogStream = createStream(
-  (time) => {
-    if (!time) return 'today.log';
-    const date = new Date(time);
+  () => {
+    const date = new Date();
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0');
     const dd = String(date.getDate()).padStart(2, '0');
